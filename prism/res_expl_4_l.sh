b6eeadab37d45c45a92e6c12a86f1206b21ac500
aissim <Aistis.Simaitis@cs.ox.ac.uk>
2011-10-06 11:18:27 +0000

    git-svn-id: https://www.prismmodelchecker.org/svn/prism/prism/branches/prism-games@3864 bbc10eb1-c90d-0410-af57-cb519fbb1720

---
+++ prism/res_expl_4_l.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+rm -f examples/games/CDM/experiments/no_commit/results/res_expl_4_l=0g=1e=1.txt
+PRISM_JAVAMAXMEM=16g bin/prism -ex examples/games/CDM/experiments/no_commit/cdm4032.smg examples/games/CDM/experiments/no_commit/props_expl_4.pctl -const Pexp=0.5,Q1=1.0,Q2=0.5,Q3=0.25,gamma=1,eta=1,lambda=0 > examples/games/CDM/experiments/no_commit/results/res_expl_4_l=0g=1e=1.txt
+rm -f examples/games/CDM/experiments/no_commit/results/res_expl_4_l=1g=1e=1.txt
+PRISM_JAVAMAXMEM=16g bin/prism -ex examples/games/CDM/experiments/no_commit/cdm4032.smg examples/games/CDM/experiments/no_commit/props_expl_4.pctl -const Pexp=0.5,Q1=1.0,Q2=0.5,Q3=0.25,gamma=1,eta=1,lambda=1 > examples/games/CDM/experiments/no_commit/results/res_expl_4_l=1g=1e=1.txt
+rm -f examples/games/CDM/experiments/no_commit/results/res_expl_4_l=2g=1e=1.txt
+PRISM_JAVAMAXMEM=16g bin/prism -ex examples/games/CDM/experiments/no_commit/cdm4032.smg examples/games/CDM/experiments/no_commit/props_expl_4.pctl -const Pexp=0.5,Q1=1.0,Q2=0.5,Q3=0.25,gamma=1,eta=1,lambda=2 > examples/games/CDM/experiments/no_commit/results/res_expl_4_l=2g=1e=1.txt
+rm -f examples/games/CDM/experiments/no_commit/results/res_expl_4_l=3g=1e=1.txt
+PRISM_JAVAMAXMEM=16g bin/prism -ex examples/games/CDM/experiments/no_commit/cdm4032.smg examples/games/CDM/experiments/no_commit/props_expl_4.pctl -const Pexp=0.5,Q1=1.0,Q2=0.5,Q3=0.25,gamma=1,eta=1,lambda=3 > examples/games/CDM/experiments/no_commit/results/res_expl_4_l=3g=1e=1.txt
+rm -f examples/games/CDM/experiments/no_commit/results/res_expl_4_l=4g=1e=1.txt
+PRISM_JAVAMAXMEM=16g bin/prism -ex examples/games/CDM/experiments/no_commit/cdm4032.smg examples/games/CDM/experiments/no_commit/props_expl_4.pctl -const Pexp=0.5,Q1=1.0,Q2=0.5,Q3=0.25,gamma=1,eta=1,lambda=4 > examples/games/CDM/experiments/no_commit/results/res_expl_4_l=4g=1e=1.txt
+rm -f examples/games/CDM/experiments/no_commit/results/res_expl_4_l=5g=1e=1.txt
+PRISM_JAVAMAXMEM=16g bin/prism -ex examples/games/CDM/experiments/no_commit/cdm4032.smg examples/games/CDM/experiments/no_commit/props_expl_4.pctl -const Pexp=0.5,Q1=1.0,Q2=0.5,Q3=0.25,gamma=1,eta=1,lambda=5 > examples/games/CDM/experiments/no_commit/results/res_expl_4_l=5g=1e=1.txt
+
+
