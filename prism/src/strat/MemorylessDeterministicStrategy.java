34ce4ecda3b0c8e8ecef815f81dd05becd333286
aissim <Aistis.Simaitis@cs.ox.ac.uk>
2012-04-26 13:39:28 +0000

    Updated strategy implementations
    git-svn-id: https://www.prismmodelchecker.org/svn/prism/prism/branches/prism-games@5046 bbc10eb1-c90d-0410-af57-cb519fbb1720

--- prism/src/strat/MemorylessDeterministicStrategy.java
+++ prism/src/strat/MemorylessDeterministicStrategy.java
@@ -1,12 +1,23 @@
 package strat;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map.Entry;
+
+import cern.colt.Arrays;
+import prism.PrismFileLog;
+import prism.PrismLog;
 import explicit.Distribution;
+import explicit.MDPSimple;
+import explicit.MDPSparse;
+import explicit.Model;
 
 /**
  * Implementation of the memoryless deterministic strategy
  * 
  * @author aistis
- *
+ * 
  */
 public class MemorylessDeterministicStrategy implements Strategy {
 
@@ -36,16 +47,95 @@ public class MemorylessDeterministicStrategy implements Strategy {
 	@Override
 	public Distribution getNextMove(int state)
 			throws InvalidStrategyStateException {
-		
-		if(choices == null || state >= choices.length || state < 0)
-			throw new InvalidStrategyStateException("Strategy not defined for state " + state +".");
-		
+
+		if (choices == null || state >= choices.length || state < 0)
+			throw new InvalidStrategyStateException(
+					"Strategy not defined for state " + state + ".");
+
 		return choices[state];
 	}
 
 	@Override
 	public void reset() {
 		// do nothing
+	}
+
+	@Override
+	public Model buildProduct(Model model) {
+		
+		// checking for supported model types
+		if (model instanceof MDPSimple) {
+			return this.buildProductMDPSimple((MDPSimple) model);
+		}
+		if (model instanceof MDPSparse) {
+			return this.buildProductMDPSparse((MDPSparse) model);
+		}
+
+		throw new UnsupportedOperationException(
+				"The product building is not supported for this class of models");
+
+	}
+
+	@Override
+	public void exportToFile(String file) {
+		// Print adversary
+		PrismLog out = new PrismFileLog(file);
+		out.print("Adv:");
+		for (int i = 0; i < choices.length; i++) {
+			out.print(" " + i + ":" + choices[i]);
+		}
+		out.println();
+	}
+
+	@Override
+	public String toString() {
+		return Arrays.toString(choices);
+	}
+
+	/**
+	 * Builds product of MDPSimple and a given strategy
+	 * 
+	 * @param model
+	 *            model
+	 */
+	public MDPSimple buildProductMDPSimple(MDPSimple model) {
+		MDPSimple mdp = new MDPSimple(model);
+		int n = mdp.getNumStates();
+		int c;
+		Distribution distr;
+
+		for (int s = 0; s < n; s++) {
+
+			// getting the choice of a strategy for this state
+			try {
+				c = this.getNextMove(s).keySet().iterator().next();
+
+				// if for adversary choice is undefined, taking the first one as
+				// default
+				if (c < 0)
+					c = 0;
+			} catch (InvalidStrategyStateException e) {
+				// strategy undefined for this state -- keep calm and carry
+				// on
+				continue;
+			}
+
+			// replacing the choices with the one prescribed by the strategy
+			distr = mdp.getChoice(s, c);
+			mdp.clearState(s);
+			mdp.addChoice(s, distr);
+		}
+		return mdp;
+	}
+
+	/**
+	 * Builds product of MDPSparse and a given strategy
+	 * 
+	 * @param model
+	 *            model
+	 */
+	public MDPSparse buildProductMDPSparse(MDPSparse model) {
+		return new MDPSparse(buildProductMDPSimple(new MDPSimple(model)));
 	}
 
 	/**
@@ -55,4 +145,5 @@ public class MemorylessDeterministicStrategy implements Strategy {
 		// TODO Auto-generated method stub
 
 	}
+
 }
--- prism/src/strat/Strategy.java
+++ prism/src/strat/Strategy.java
@@ -1,60 +1,92 @@
 package strat;
 
+import java.io.File;
+
+import prism.PrismException;
 import explicit.Distribution;
+import explicit.Model;
 
 /**
  * 
- * Generic interface to use the strategies based on stochastic strategy automata [?].
+ * Generic interface to use the strategies based on stochastic strategy automata
+ * [?].
  * 
- * The strategy should be used interactively. The typical usage pattern would be:
- * 1) In the initial state of the model call init method.
- * 2) Call nextMove method with the initial state as parameter.
- * 3) Choose the successor according to the distribution.
- * 4) Once entered the successor state, call updateMemory method with the action taken
- * in the previous state and the successor state.
+ * The strategy should be used interactively. The typical usage pattern would
+ * be: 1) In the initial state of the model call init method. 2) Call nextMove
+ * method with the initial state as parameter. 3) Choose the successor according
+ * to the distribution. 4) Once entered the successor state, call updateMemory
+ * method with the action taken in the previous state and the successor state.
  * 5) Repeat steps 2-4 forever.
  * 
- * Strategy could also used to obtain the choice after a (strategy-compatible) history:
- * 1) Call reset method to reset the strategy.
- * 2) Follow the previous instructions 1-5 except in step 3, choose the successor
- * based on the given history. 
+ * Strategy could also used to obtain the choice after a (strategy-compatible)
+ * history: 1) Call reset method to reset the strategy. 2) Follow the previous
+ * instructions 1-5 except in step 3, choose the successor based on the given
+ * history.
  * 
  * @author aistis
- *
+ * 
  */
 public interface Strategy {
 
 	/**
 	 * Initialises memory based on a state
 	 * 
-	 * @param state initial state
-	 * @throws InvalidStrategyStateException if the initial distribution
-	 * function is undefined for the given state
+	 * @param state
+	 *            initial state
+	 * @throws InvalidStrategyStateException
+	 *             if the initial distribution function is undefined for the
+	 *             given state
 	 */
 	public void init(int state) throws InvalidStrategyStateException;
-	
+
 	/**
 	 * Updates memory
 	 * 
-	 * @param action action taken in the previous states
-	 * @param state the current state
-	 * @throws InvalidStrategyStateException if memory update function is not defined
-	 * for the given action, state and the current strategy's memory state.
+	 * @param action
+	 *            action taken in the previous states
+	 * @param state
+	 *            the current state
+	 * @throws InvalidStrategyStateException
+	 *             if memory update function is not defined for the given
+	 *             action, state and the current strategy's memory state.
 	 */
-	public void updateMemory(int action, int state)  throws InvalidStrategyStateException;
+	public void updateMemory(int action, int state)
+			throws InvalidStrategyStateException;
 
 	/**
 	 * Next move function
 	 * 
-	 * @param state current state
-	 * @return the distribution on actions prescribed by the strategy in a state.
-	 * @throws InvalidStrategyStateException if next move function is undefined
-	 * for the given state in current strategy's memory state.
+	 * @param state
+	 *            current state
+	 * @return the distribution on actions prescribed by the strategy in a
+	 *         state.
+	 * @throws InvalidStrategyStateException
+	 *             if next move function is undefined for the given state in
+	 *             current strategy's memory state.
 	 */
-	public Distribution getNextMove(int state) throws InvalidStrategyStateException;
-	
+	public Distribution getNextMove(int state)
+			throws InvalidStrategyStateException;
+
 	/**
 	 * Resets the strategy to uninitialised state
 	 */
 	public void reset();
+
+	/**
+	 * Exports adversary to a given file
+	 * 
+	 * @param file
+	 *            file name to which adversary will be exported
+	 */
+	public void exportToFile(String file);
+
+	/**
+	 * Builds the product of the model and the strategy..
+	 * 
+	 * @param model
+	 *            The model for which the strategy is defined.
+	 * 
+	 */
+	public Model buildProduct(Model model);
+
 }
