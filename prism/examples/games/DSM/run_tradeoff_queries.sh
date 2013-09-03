f4b7ac40ddb42b11ac6fe3ef7c2427a1f302b93b
aissim <Aistis.Simaitis@cs.ox.ac.uk>
2013-09-03 19:58:15 +0000

    adding stuff for experiments
    git-svn-id: https://www.prismmodelchecker.org/svn/prism/prism/branches/prism-games@7319 bbc10eb1-c90d-0410-af57-cb519fbb1720

---
+++ prism/examples/games/DSM/run_tradeoff_queries.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+
+
+
+../../../../bin/prism value_tradeoff/dsm2304.smg -ex -pctl '<<1>> R{"value1"}max=? [F time=max_time]' >> examples/games/DSM/experiments/value_tradeoff/results/res2.txt
+../../../../bin/prism value_tradeoff/dsm2304.smg -ex -pctl '<<1,2>> R{"value12"}max=? [F time=max_time]' >> examples/games/DSM/experiments/value_tradeoff/results/res2.txt
+
+
+
+
+../../../../bin/prism value_tradeoff/dsm3304.smg -ex -pctl '<<1>> R{"value1"}max=? [F time=max_time]' >> examples/games/DSM/experiments/value_tradeoff/results/res3.txt
+../../../../bin/prism value_tradeoff/dsm3304.smg -ex -pctl '<<1,2>> R{"value12"}max=? [F time=max_time]' >> examples/games/DSM/experiments/value_tradeoff/results/res3.txt
+../../../../bin/prism value_tradeoff/dsm3304.smg -ex -pctl '<<1,2,3>> R{"value123"}max=? [F time=max_time]' >> examples/games/DSM/experiments/value_tradeoff/results/res3.txt
+
+
+
+
+../../../../bin/prism value_tradeoff/dsm4304.smg -ex -pctl '<<1>> R{"value1"}max=? [F time=max_time]' >> examples/games/DSM/experiments/value_tradeoff/results/res4.txt
+../../../../bin/prism value_tradeoff/dsm4304.smg -ex -pctl '<<1,2>> R{"value12"}max=? [F time=max_time]' >> examples/games/DSM/experiments/value_tradeoff/results/res4.txt
+../../../../bin/prism value_tradeoff/dsm4304.smg -ex -pctl '<<1,2,3>> R{"value123"}max=? [F time=max_time]' >> examples/games/DSM/experiments/value_tradeoff/results/res4.txt
+../../../../bin/prism value_tradeoff/dsm4304.smg -ex -pctl '<<1,2,3,4>> R{"value1234"}max=? [F time=max_time]' >> examples/games/DSM/experiments/value_tradeoff/results/res4.txt
+
+
+
+
+PRISM_JAVAMAXMEM=8g ../../../../bin/prism value_tradeoff/dsm5304.smg -ex -pctl '<<1>> R{"value1"}max=? [F time=max_time]' >> examples/games/DSM/experiments/value_tradeoff/results/res5.txt
+PRISM_JAVAMAXMEM=8g ../../../../bin/prism value_tradeoff/dsm5304.smg -ex -pctl '<<1,2>> R{"value12"}max=? [F time=max_time]' >> examples/games/DSM/experiments/value_tradeoff/results/res5.txt
+PRISM_JAVAMAXMEM=8g ../../../../bin/prism value_tradeoff/dsm5304.smg -ex -pctl '<<1,2,3>> R{"value123"}max=? [F time=max_time]' >> examples/games/DSM/experiments/value_tradeoff/results/res5.txt
+PRISM_JAVAMAXMEM=8g ../../../../bin/prism value_tradeoff/dsm5304.smg -ex -pctl '<<1,2,3,4>> R{"value1234"}max=? [F time=max_time]' >> examples/games/DSM/experiments/value_tradeoff/results/res5.txt
+PRISM_JAVAMAXMEM=8g ../../../../bin/prism value_tradeoff/dsm5304.smg -ex -pctl '<<1,2,3,4,5>> R{"value12345"}max=? [F time=max_time]' >> examples/games/DSM/experiments/value_tradeoff/results/res5.txt
+
+
+
+
+PRISM_JAVAMAXMEM=16g ../../../../bin/prism value_tradeoff/dsm6304.smg -ex -pctl '<<1>> R{"value1"}max=? [F time=max_time]' >> examples/games/DSM/experiments/value_tradeoff/results/res6.txt
+PRISM_JAVAMAXMEM=16g ../../../../bin/prism value_tradeoff/dsm6304.smg -ex -pctl '<<1,2>> R{"value12"}max=? [F time=max_time]'  >> examples/games/DSM/experiments/value_tradeoff/results/res6.txt
+PRISM_JAVAMAXMEM=16g ../../../../bin/prism value_tradeoff/dsm6304.smg -ex -pctl '<<1,2,3>> R{"value123"}max=? [F time=max_time]'  >> examples/games/DSM/experiments/value_tradeoff/results/res6.txt
+PRISM_JAVAMAXMEM=16g ../../../../bin/prism value_tradeoff/dsm6304.smg -ex -pctl '<<1,2,3,4>> R{"value1234"}max=? [F time=max_time]'  >> examples/games/DSM/experiments/value_tradeoff/results/res6.txt
+PRISM_JAVAMAXMEM=16g ../../../../bin/prism value_tradeoff/dsm6304.smg -ex -pctl '<<1,2,3,4,5>> R{"value12345"}max=? [F time=max_time]'  >> examples/games/DSM/experiments/value_tradeoff/results/res6.txt
+PRISM_JAVAMAXMEM=16g ../../../../bin/prism value_tradeoff/dsm6304.smg -ex -pctl '<<1,2,3,4,5,6>> R{"value123456"}max=? [F time=max_time]'  >> examples/games/DSM/experiments/value_tradeoff/results/res6.txt
+
+
+
+
+PRISM_JAVAMAXMEM=32g ../../../../bin/prism value_tradeoff/dsm7304.smg -ex -pctl '<<1>> R{"value1"}max=? [F time=max_time]' >> examples/games/DSM/experiments/value_tradeoff/results/res7.txt
+PRISM_JAVAMAXMEM=32g ../../../../bin/prism value_tradeoff/dsm7304.smg -ex -pctl '<<1,2>> R{"value12"}max=? [F time=max_time]' >> examples/games/DSM/experiments/value_tradeoff/results/res7.txt
+PRISM_JAVAMAXMEM=32g ../../../../bin/prism value_tradeoff/dsm7304.smg -ex -pctl '<<1,2,3>> R{"value123"}max=? [F time=max_time]' >> examples/games/DSM/experiments/value_tradeoff/results/res7.txt
+PRISM_JAVAMAXMEM=32g ../../../../bin/prism value_tradeoff/dsm7304.smg -ex -pctl '<<1,2,3,4>> R{"value1234"}max=? [F time=max_time]' >> examples/games/DSM/experiments/value_tradeoff/results/res7.txt
+PRISM_JAVAMAXMEM=32g ../../../../bin/prism value_tradeoff/dsm7304.smg -ex -pctl '<<1,2,3,4,5>> R{"value12345"}max=? [F time=max_time]' >> examples/games/DSM/experiments/value_tradeoff/results/res7.txt
+PRISM_JAVAMAXMEM=32g ../../../../bin/prism value_tradeoff/dsm7304.smg -ex -pctl '<<1,2,3,4,5,6>> R{"value123456"}max=? [F time=max_time]' >> examples/games/DSM/experiments/value_tradeoff/results/res7.txt
+PRISM_JAVAMAXMEM=32g ../../../../bin/prism value_tradeoff/dsm7304.smg -ex -pctl '<<1,2,3,4,5,6,7>> R{"value1234567"}max=? [F time=max_time]' >> examples/games/DSM/experiments/value_tradeoff/results/res7.txt
+
+
