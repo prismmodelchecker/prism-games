10ce37a0f45a8eabf24ea5c959ac9194fca80c21
Dave Parker <dave.x.parker@gmail.com>
2015-09-22 21:54:32 +0000

    Merge trunk changes.
    git-svn-id: https://www.prismmodelchecker.org/svn/prism/prism/branches/prism-games@10716 bbc10eb1-c90d-0410-af57-cb519fbb1720

--- prism/src/userinterface/GUIPrism.java
+++ prism/src/userinterface/GUIPrism.java
@@ -381,7 +381,7 @@ public class GUIPrism extends JFrame
 		}
 
 		setJMenuBar(menuBar);
-		setTitle("PRISM-games " + prism.getVersion());
+		setTitle(Prism.getToolName() + " " + Prism.getVersion());
 		setDefaultCloseOperation(javax.swing.JFrame.DO_NOTHING_ON_CLOSE);
 		addWindowListener(new java.awt.event.WindowAdapter()
 		{
