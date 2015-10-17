3aeca8cb71191961fe7eea0ac66f757e13a9831f
Dave Parker <dave.x.parker@gmail.com>
2015-10-17 07:50:14 +0000

    Improve detection of PPL slightly and use ppl-config for this as a first option.
    git-svn-id: https://www.prismmodelchecker.org/svn/prism/prism/branches/prism-games@10793 bbc10eb1-c90d-0410-af57-cb519fbb1720

--- prism/src/scripts/findppl.sh
+++ prism/src/scripts/findppl.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-if type rpm &> /dev/null; then
-  echo `rpm -qi --filesbypkg ppl-java | grep "libppl_java" | awk -F'/' '{OFS="/";$1="";$(NF-1)="";$NF="";print}' | sed 's/.$//'`
+# Detection of PPL:
+# * By default, try to use the ppl-config command (if it's in the path)
+# * If that fails, try looking for an rpm
+
+if command ppl-config &> /dev/null; then
+  echo `ppl-config -l`
 else
-  echo `ppl-config -p`/ppl/lib
+  if command rpm &> /dev/null; then
+	echo `rpm -qi --filesbypkg ppl-java | grep "libppl_java" | awk -F'/' '{OFS="/";$1="";$(NF-1)="";$NF="";print}' | sed 's/.$//'`
+  fi
 fi
