27c99325b33c5faa4b81bf173aff283e80b00fbb
Dave Parker <dave.x.parker@gmail.com>
2016-08-23 23:34:17 +0000

    Initial tidy up of Strategy class.
    git-svn-id: https://www.prismmodelchecker.org/svn/prism/prism/branches/prism-games@11701 bbc10eb1-c90d-0410-af57-cb519fbb1720

--- prism/src/strat/Strategy.java
+++ prism/src/strat/Strategy.java
@@ -1,15 +1,39 @@
+//==============================================================================
+//	
+//	Copyright (c) 2002-
+//	Authors:
+//	* Dave Parker <d.a.parker@cs.bham.ac.uk> (University of Birmingham/Oxford)
+//	* Aistis Simaitis <aistis.aimaitis@cs.ox.ac.uk> (University of Oxford)
+//	
+//------------------------------------------------------------------------------
+//	
+//	This file is part of PRISM.
+//	
+//	PRISM is free software; you can redistribute it and/or modify
+//	it under the terms of the GNU General Public License as published by
+//	the Free Software Foundation; either version 2 of the License, or
+//	(at your option) any later version.
+//	
+//	PRISM is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU General Public License for more details.
+//	
+//	You should have received a copy of the GNU General Public License
+//	along with PRISM; if not, write to the Free Software Foundation,
+//	Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//	
+//==============================================================================
+
 package strat;
 
-import prism.PrismException;
-import prism.PrismLog;
 import explicit.Distribution;
 import explicit.Model;
-import parser.Values;
+import prism.PrismException;
+import prism.PrismLog;
 
 /**
- * 
- * Generic interface to use the strategies based on stochastic strategy automata
- * [?].
+ * Interface for classes to store strategies (for MDPs, games, etc.)
  * 
  * The strategy should be used interactively. The typical usage pattern would
  * be: 1) In the initial state of the model call init method. 2) Call nextMove
@@ -22,9 +46,6 @@ import parser.Values;
  * history: 1) Call reset method to reset the strategy. 2) Follow the previous
  * instructions 1-5 except in step 3, choose the successor based on the given
  * history.
- * 
- * @author aistis
- * 
  */
 public interface Strategy
 {
@@ -32,49 +53,80 @@ public interface Strategy
 	public enum Choice {
 		INDEX, ACTION, UNKNOWN, ARBITRARY, UNREACHABLE;
 	};
+
+	// Getters for basic strategy info
 	
 	/**
-	 * Initialises memory based on a state
-	 * 
-	 * @param state
-	 *            initial state
-	 * @throws InvalidStrategyStateException
-	 *             if the initial distribution function is undefined for the
-	 *             given state
+	 * Get a textual description of the strategy.
+	 */
+	public String getInfo();
+
+	/**
+	 * Get the size of the memory of the strategy.
+	 */
+	public int getMemorySize();
+
+	/**
+	 * Get the type of the strategy, as a string.
+	 */
+	public String getType();
+
+	/**
+	 * Returns a textual description giving key information about the strategy (ideally, human readable).
+	 */
+	public String getDescription();
+
+	// Setters for basic strategy info
+	
+	/**
+	 * Set a textual description for the strategy.
+	 * @param info String describing the strategy.
+	 */
+	public void setInfo(String info);
+
+	// Methods for interactive querying of strategy
+	
+	/**
+	 * Initialise the strategy (set its memory) based on an initial model state.
+	 * @param state Initial model state
+	 * @throws InvalidStrategyStateException if the initial distribution function is undefined for the given state
 	 */
 	public void init(int state) throws InvalidStrategyStateException;
 
 	/**
-	 * Updates memory
-	 * 
-	 * @param action
-	 *            action taken in the previous states
-	 * @param state
-	 *            the current state
-	 * @throws InvalidStrategyStateException
-	 *             if memory update function is not defined for the given
-	 *             action, state and the current strategy's memory state.
+	 * Update the strategy (update its memory) based on an action and next state from the model.
+	 * @param action Action taken in the previous model state
+	 * @param state The next model state
+	 * @throws InvalidStrategyStateException if memory update function is not defined for the given action, state and the current strategy's memory state
 	 */
 	public void updateMemory(int action, int state) throws InvalidStrategyStateException;
 
 	/**
-	 * Next move function
-	 * 
-	 * @param state
-	 *            current state
-	 * @return the distribution on actions prescribed by the strategy in a
-	 *         state.
-	 * @throws InvalidStrategyStateException
-	 *             if next move function is undefined for the given state in
-	 *             current strategy's memory state.
+	 * Get the next move (distribution over actions) for a given model state.
+	 * @param state Current model state
+	 * @throws InvalidStrategyStateException if next move function is undefined for the given state in current strategy's memory state
 	 */
 	public Distribution getNextMove(int state) throws InvalidStrategyStateException;
 
 	/**
-	 * Resets the strategy to uninitialised state
+	 * Get the current memory status of the strategy.
+	 */
+	public Object getCurrentMemoryElement();
+
+	/**
+	 * Set the current memory status of the strategy to the one provided.
+	 * @param memory Memory element representing the state of the strategy
+	 * @throws InvalidStrategyStateException if the memory element is not recognised by the strategy
+	 */
+	public void setMemory(Object memory) throws InvalidStrategyStateException;
+
+	/**
+	 * Reset the strategy to an uninitialised state.
 	 */
 	public void reset();
 
+	// Export methods
+	
 	/**
 	 * Exports adversary to a given file
 	 * 
@@ -83,6 +135,8 @@ public interface Strategy
 	 */
 	public void exportToFile(String file);
 
+	// Other methods
+	
 	/**
 	 * Builds the product of the model and the strategy. The product is built by
 	 * adding extra integer variable to the state to represent the memory state
@@ -97,58 +151,6 @@ public interface Strategy
 	public Model buildProduct(Model model) throws PrismException;
 
 	/**
-	 * Get textual description of the strategy
-	 * 
-	 * @return the textual description of the strategy
-	 */
-	public String getInfo();
-
-	/**
-	 * Set textual description of the strategy
-	 * 
-	 * @param info
-	 *            strategy information
-	 */
-	public void setInfo(String info);
-
-	/**
-	 * Returns the size of memory of the strategy.
-	 * 
-	 * @return size of memory
-	 */
-	public int getMemorySize();
-
-	/**
-	 * Returns strategy type
-	 * 
-	 * @return type of the strategy
-	 */
-	public String getType();
-
-	/**
-	 * Returns the current memory element that fully describes state of the
-	 * strategy
-	 * 
-	 * @return
-	 */
-	public Object getCurrentMemoryElement();
-
-	/**
-	 * Updates the strategy's state to the one provided
-	 * 
-	 * @param memory
-	 *            memory element representing the state of the strategy
-	 * @throws InvalidStrategyStateException
-	 *             if the memory element is not recognised by the strategy
-	 */
-	public void setMemory(Object memory) throws InvalidStrategyStateException;
-
-	/**
-	 * Returns a textual description giving key information about the strategy (ideally, human readable).
-	 */
-	public String getDescription();
-
-	/**
 	 * Returns the initial memory state of the strategy for the state. It is
 	 * required by the model checker to determine which states can be treated as
 	 * initial in the product.
@@ -161,7 +163,6 @@ public interface Strategy
 	 */
 	public int getInitialStateOfTheProduct(int s);
 
-	
 	//	/**
 	//	 * Retrieve the expected value that this strategy will achieve from it's
 	//	 * current state
@@ -178,27 +179,27 @@ public interface Strategy
 	//	public double getExpectedValue(int action, int state);
 
 	// NEW METHODS:
-	
+
 	/**
 	 * Export the strategy to a PrismLog, displaying strategy choices as action names.
 	 */
 	public void exportActions(PrismLog out);
-	
+
 	/**
 	 * Export the strategy to a PrismLog, displaying strategy choices as indices.
 	 */
 	public void exportIndices(PrismLog out);
-	
+
 	/**
 	 * Export the model induced by this strategy to a PrismLog.
 	 */
 	public void exportInducedModel(PrismLog out);
-	
+
 	/**
 	 * Export the strategy to a dot file (of the model showing the strategy).
 	 */
 	public void exportDotFile(PrismLog out);
-	
+
 	/**
 	 * Initialise the strategy, based on an initial model state.
 	 * @param s Initial state of the model
@@ -211,12 +212,12 @@ public interface Strategy
 	 * @param s The new state of the model
 	 */
 	public void update(Object action, int s);
-	
+
 	/**
 	 * Get the action chosen by the strategy in the current state (assuming it is deterministic). 
 	 */
 	public Object getChoiceAction();
-	
+
 	/**
 	 * Clear storage of the strategy.
 	 */
