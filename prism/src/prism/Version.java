78da2130c21ea3689e7d023513aeaf7a813d4239
Dave Parker <dave.x.parker@gmail.com>
2020-05-21 00:06:03 +0100

    Version (3.0).

--- prism/src/prism/Version.java
+++ prism/src/prism/Version.java
@@ -5,7 +5,7 @@ public class Version
 	/** PRISM extension version (e.g. "2.0"). Just a single string (will be extracted by scripts). */
 	public static String versionExtensionString = "3.0";
 	/** Optional PRISM extension version suffix (e.g. "", "dev", "beta"). Just a single string (will be extracted by scripts). */
-	public static String versionExtensionSuffixString = "beta";
+	public static String versionExtensionSuffixString = "";
 	
 	/** PRISM version (e.g. "4.0.3"). Just a single string (will be extracted by scripts). */
 	public static String versionString = "4.6";
