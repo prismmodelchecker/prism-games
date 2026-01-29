1f05bcca75f1574c89774cefc582abb9f9e211af
Dave Parker <dave.x.parker@gmail.com>
2026-01-29 19:47:07 +0000

    Update to version 3.2.2.

--- prism/src/prism/Version.java
+++ prism/src/prism/Version.java
@@ -3,7 +3,7 @@ package prism;
 public class Version
 {
 	/** PRISM extension version (e.g. "2.0"). Just a single string (will be extracted by scripts). */
-	public static String versionExtensionString = "3.2.1";
+	public static String versionExtensionString = "3.2.2";
 	/** Optional PRISM extension version suffix (e.g. "", "dev", "beta"). Just a single string (will be extracted by scripts). */
 	public static String versionExtensionSuffixString = "";
 	
